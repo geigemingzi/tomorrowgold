7be2684ed8ec4009eca38b44b863a7e27ffb5940
zhaodong <457450738@qq.com>
2018-10-20 17:42:47 +0800

    跨域设置

--- wgoldwed/config/index.js
+++ wgoldwed/config/index.js
@@ -10,7 +10,18 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://restapp.tomorrowgold.com',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      },
+      '/mongo': {
+        target: 'http://10.3.137.2/1234',
+        changeOrigin: true,
+        pathRewrite: { '^/mongo': '' }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
